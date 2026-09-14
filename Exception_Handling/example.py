e92b28cfd17c1a085cb0978b10a51a1e0cd993fd
Your Name <your-email@example.com>
2026-09-15 01:40:37 +0530

    add exception handling

---
+++ Exception_Handling/example.py
@@ -0,0 +1,11 @@
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
